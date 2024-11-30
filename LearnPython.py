645e03175543a86d049da7efc2ea4398bd57a858
thejulians54 <salmanca296@gmail.com>
2024-11-29 23:20:21 -0500

    operators

---
+++ LearnPython.py
@@ -0,0 +1,8 @@
+print (10 / 3) #realizar la division 10/3
+print (10 // 3) #realizar la division entera 10/3
+print (10 ** 3) #elevar 10 a la potencia 3
+print (10 % 3) #sacar el resto de la division 10/3
+print (10 + 3) #sumar 10 y 3
+print (10 - 3) #restar 10 y 3
+print (10 * 3) #multiplicar 10 y 3
+print (10 / 3.0) #realizar la division 10/3.0
